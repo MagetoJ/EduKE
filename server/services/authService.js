f81e322a7994adafa7e84d6f55840c2221d685cb
MagetoJ <jmageto@statbricks.com>
2025-11-22 19:13:06 +0300

    env changes

--- server/services/authService.js
+++ server/services/authService.js
@@ -10,11 +10,16 @@ const crypto = require('crypto');
 const { query, transaction } = require('../db/connection');
 require('dotenv').config();
 
-const SALT_ROUNDS = 10;
-const JWT_SECRET = process.env.JWT_SECRET;
-const JWT_REFRESH_SECRET = process.env.JWT_REFRESH_SECRET;
-const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN || '15m';
-const JWT_REFRESH_EXPIRES_IN = process.env.JWT_REFRESH_EXPIRES_IN || '7d';
+const {
+  JWT_SECRET,
+  JWT_REFRESH_SECRET,
+  JWT_ACCESS_EXPIRES_IN,
+  SALT_ROUNDS
+} = require('../config');
+
+const JWT_EXPIRES_IN = JWT_ACCESS_EXPIRES_IN;
+const JWT_REFRESH_EXPIRES_IN = '7d';
+
 
 /**
  * Register a new school with admin user
